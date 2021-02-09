3e1894d2262aa3bf0afaab8c0e404f54df76f5f7
Dwi Pamungkas <dw.pamungkas@gmail.com>
2021-02-09 12:57:15 +0700

    First Commit Source Code

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+<!-- Bootstrap JS Bye Jquery-->
+<script src="js/bootstrap.bundle.min.js"></script>
+
+<script>
+
+//YOUR CODE HERE
+
+</script>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php require 'inc/header.php'; ?>
+        <title>Home | My Bootstrap 5 Starter Template</title>
+    </head>
+    <body>
+    <?php require 'inc/top-menu.php'; ?>
+    <!--CONTENT -->
+
+
+
+
+
+    <!--END CONTENT -->
+    <?php require 'inc/footer.php'; ?>
+    </body>
+</html>
+
+    
